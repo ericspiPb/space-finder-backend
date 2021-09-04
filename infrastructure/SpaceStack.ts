ac859db297d0e792813385c77c6091990ec9f6d6
Eric Chan <eric88ordinary@gmail.com>
2021-09-04 21:16:19 +0800

    basic hello from lamda

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,10 +1,16 @@
-import { Stack, StackProps } from '@aws-cdk/core';
-import { Construct } from 'constructs';
+import { Construct, Stack, StackProps } from '@aws-cdk/core';
+import { Code, Function as LamdaFunction, Runtime } from '@aws-cdk/aws-lambda';
 
 export class SpaceStack extends Stack {
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    new LamdaFunction(this, 'helloLamda', {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset('services'),
+      handler: 'hello.main',
+    });
   }
 
 }
