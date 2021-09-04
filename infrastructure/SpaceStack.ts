ec4b4b41ddc47d5cdc6a08e68a0bd176db355bb2
Eric Chan <eric88ordinary@gmail.com>
2021-09-04 21:28:48 +0800

    api gateway integration with lamda and test req

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,16 +1,24 @@
 import { Construct, Stack, StackProps } from '@aws-cdk/core';
 import { Code, Function as LamdaFunction, Runtime } from '@aws-cdk/aws-lambda';
+import { LambdaIntegration, RestApi } from '@aws-cdk/aws-apigateway';
 
 export class SpaceStack extends Stack {
+
+  private api = new RestApi(this, 'SpaceApi');
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new LamdaFunction(this, 'helloLamda', {
+    const helloLamda = new LamdaFunction(this, 'helloLamda', {
       runtime: Runtime.NODEJS_14_X,
       code: Code.fromAsset('services'),
       handler: 'hello.main',
     });
+
+    // Hello Api lamda integration:
+    const helloLamdaIntegration = new LambdaIntegration(helloLamda);
+    const helloLamdaResource = this.api.root.addResource('hello');
+    helloLamdaResource.addMethod('GET', helloLamdaIntegration);
   }
 
 }
