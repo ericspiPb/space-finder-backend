ba832c8a187384c5a4bb36bd7204331d58cf544d
Eric Chan <eric88ordinary@gmail.com>
2021-09-06 21:37:07 +0800

    fix status code bug

--- services/node-lambda/hello.ts
+++ services/node-lambda/hello.ts
@@ -2,7 +2,7 @@ import * as uuid from 'uuid';
 
 async function handler(event: any, context: any) {
   return {
-    statusCode: 20,
+    statusCode: 200,
     body: 'Hello from lambda!' + uuid.v4(),
   };
 }
